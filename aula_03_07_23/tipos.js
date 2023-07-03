916ec3b9c84724f070ade656fc8bb8746e26a99d
N1ck49871 <nickalves0498@outlook.com>
2023-07-03 13:15:59 -0300

    variaveis javascript

---
+++ aula_03_07_23/tipos.js
@@ -0,0 +1,13 @@
+// Tipagem Estática -> Definição de um tipo a cada variável (Java, C, C++, TypeScript)
+// Tipagem Dinâmica -> O tipo da variável é definido de acordo com seu valor(JavaScript, Python)
+
+//typeof -> Operador que mostra o tipo de variavel
+
+let variavel = 22;
+console.log(typeof variavel);
+
+variavel = "teste";
+console.log(typeof variavel);
+
+variavel = true;
+console.log(typeof variavel);
---
+++ aula_03_07_23/variaveis.js
@@ -0,0 +1,40 @@
+// JavaSript -> Linguagem de Programação (Interpretado)
+// Java -> Linguagem de Programação (Compilado)
+
+/*
+  Comentarios De Múltiplas Linhas
+*/
+// Comentario de linha Única
+
+// Chrome -> V8 -> Interpretador do JS
+// Node.js -> V8 + NPM (Node Package Manager)
+
+console.log("Texto"); //Imprime no terminal/console uma informação
+console.log(5);
+
+// Variaveis -> Espaço (na memória) para armazenar um valor
+let nome; //Declaração da Variavel nome
+nome = "Usuário"; // atribuição do valor à variavel
+console.log(nome);
+
+let idade = 23;
+console.log(idade);
+
+let altura = 1.75;
+console.log(altura);
+
+let cpf = '123.456.789-00';
+console.log(cpf);
+
+let casado = false;
+console.log(casado);
+
+nome = "Nicolas Alves";
+console.log(nome);
+
+idade = 24;
+console.log(idade);
+
+const pi = 3.14;
+console.log(pi);
+
