e2e6decab6b0ec38bb0fa574d282f35b04ff91ea
N1ck49871 <nickalves0498@outlook.com>
2023-07-05 09:06:28 -0300

    Aula 4/07/23

---
+++ aula_04_07_23/atividade/atividade.js
@@ -0,0 +1,34 @@
+// Atividade 1
+let numero = 4
+let calculo = numero % 2
+console.log(calculo == 0 ? "par" : "impar")
+// Atividade 2
+let num1 = 1
+let num2 = 5
+let num3 = 6
+let final = (num1 + num2 + num3) / 3
+console.log(final)
+// Atividade 3
+let numerodiv35 = 30
+let condicao = (numerodiv35 % 3 == 0) && (numerodiv35 % 5 ==0)
+console.log(condicao)
+// Atividade 4
+let base = 4
+let altura = 4
+let resultado = (base * altura) / 2
+console.log(resultado)
+// Atividade 5
+let celsius = 23
+let fahrenheit = celsius * 1.8 + 32
+console.log(fahrenheit)
+// Atividade 6
+let a = 4
+let b = 2
+let c = 0
+let delta = b ** 2 - 4 * (a * c)
+console.log(delta)
+// Atividade 7
+let idade = 17
+let calc = idade >= 18
+let result = calc ? "pode dirigir" : "nao pode dirigir"
+console.log(result)
---
+++ aula_04_07_23/operadores.js
@@ -0,0 +1,28 @@
+//  Operadores Incremento e Decremento
+/*
+  a++ -> pos incremento
+  ++a -> pre incremento
+  a-- -> pos decremento
+  --a -> pre decremento
+*/
+
+// let a = 0
+// console.log(a) // 0
+
+// console.log(a++)
+// console.log(a)
+
+// Operadores Relacionais (boolean)
+/*
+  < -> menor que
+  > -> maior que
+  <= ->menor ou igual
+  >= -> maior ou igual
+  == igual a
+  === igual a e mesmo tipo
+  != diferente
+*/
+
+let idade = 60
+let calc = (idade >= 18) && (idade <= 65)
+console.log(calc)
